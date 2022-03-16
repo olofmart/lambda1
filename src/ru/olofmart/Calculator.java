4b6bbd2b79b6788dc6f07e661c9996f31532fb8f
martynovov@yahoo.com <martynovov@yahoo.com>
2022-03-16 15:18:25 +0300

    final

--- src/ru/olofmart/Calculator.java
+++ src/ru/olofmart/Calculator.java
@@ -8,7 +8,14 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) ->
+    {
+        if (y == 0) {
+            System.out.println("деление на ноль");
+            return 0;
+        }
+        return x / y;
+    };
 
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
